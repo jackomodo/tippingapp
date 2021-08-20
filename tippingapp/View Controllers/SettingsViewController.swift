c55eaeeea3d249c292ed7d00b4c431739f0ec359
Jackson Mai <jacksonmai@gmail.com>
2021-08-20 11:12:44 -0500

    Settings UI created

---
+++ tippingapp/View Controllers/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  tippingapp
+//
+//  Created by Jackson Mai on 8/19/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideKeyboardWhenTappedAround()
+
+    }
+    
+
+
+}
