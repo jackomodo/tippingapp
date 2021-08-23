d34ef593f6f99217d7e137adc15d80c70b8f8bc2
Jackson Mai <jacksonmai@gmail.com>
2021-08-23 08:57:07 -0500

    Finished most of the UI

--- tippingapp/View Controllers/SettingsViewController.swift
+++ tippingapp/View Controllers/SettingsViewController.swift
@@ -8,13 +8,43 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    //MARK: - IBOutlets
 
+    @IBOutlet weak var firstPercentageTextField: UITextField!
+    @IBOutlet weak var secondPercentageTextField: UITextField!
+    @IBOutlet weak var thirdPercentageTextField: UITextField!
+    @IBOutlet weak var fourthPercentageTextField: UITextField!
+    @IBOutlet weak var homeButton: UIButton!
+    
+    //MARK: - Variables
+    
+    var firstPercentage = "10"
+    var secondPercentage = "15"
+    var thirdPercentage = "20"
+    var fourthPercentage = "25"
+    
+    //MARK: - Life Cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         hideKeyboardWhenTappedAround()
 
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateViews()
+        
+    }
+    
+    //MARK: - Functions
+    
+    func updateViews() {
+        firstPercentageTextField.text = firstPercentage + "%"
+        secondPercentageTextField.text = secondPercentage + "%"
+        thirdPercentageTextField.text = thirdPercentage + "%"
+        fourthPercentageTextField.text = fourthPercentage + "%"
+    }
 
 }
